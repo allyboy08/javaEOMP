ff9e5e0397d0fb23f79505a5dd4a78f7a3fb2499
allyboy08 <58805512+allyboy08@users.noreply.github.com>
2021-01-07 16:08:52 +0200

    Add files via upload

---
+++ JavaEOMP/index.js
@@ -0,0 +1,9 @@
+//alert("Welcome!")
+function myFunction() {
+  var x = document.getElementById("mysidebar");
+  if (x.className === "sidebar") {
+    x.className += " responsive";
+  } else {
+    x.className = "sidebar";
+  }
+}
