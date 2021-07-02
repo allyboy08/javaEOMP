2556851dca72049d60c1808d6de3cc1ec88ce0ac
allyboy08 <58805512+allyboy08@users.noreply.github.com>
2021-07-02 15:54:35 +0200

    updated

---
+++ type.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "QA Tester"],
+    typeSpeed: 100,
+    backspeed: 60,
+    loop: true,
+});
+var typed = new Typed(".typing2", {
+    strings: ["a Web Developers", "a QA Tester"],
+    typeSpeed: 100,
+    backspeed: 60,
+    loop: true,
+});
