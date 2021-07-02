b4e201ba30bef4bf35413f64944c4ddb29fb66e2
allyboy08 <58805512+allyboy08@users.noreply.github.com>
2021-07-02 10:40:41 +0200

    Delete index.js

--- JavaEOMP/index.js
+++
@@ -1,9 +0,0 @@
-//alert("Welcome!")
-function myFunction() {
-  var x = document.getElementById("mysidebar");
-  if (x.className === "sidebar") {
-    x.className += " responsive";
-  } else {
-    x.className = "sidebar";
-  }
-}
