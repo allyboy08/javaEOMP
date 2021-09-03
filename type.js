f25c8f7c10c57ab6469b45711dfbdd2799dad6c0
allyboy08 <58805512+allyboy08@users.noreply.github.com>
2021-09-03 15:28:44 +0200

    Update type.js

--- type.js
+++ type.js
@@ -1,11 +1,11 @@
 var typed = new Typed(".typing", {
-    strings: ["Software Developer", "QA Tester"],
+    strings: ["Web Developer", "QA Tester"],
     typeSpeed: 100,
     backspeed: 60,
     loop: true,
 });
 var typed = new Typed(".typing2", {
-    strings: ["a Software Developers", "a QA Tester"],
+    strings: ["a Web Developers", "a QA Tester"],
     typeSpeed: 100,
     backspeed: 60,
     loop: true,
